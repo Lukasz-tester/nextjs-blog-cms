de9a0535acd3de2fde48b3e61248b58561598e16
Lukasz Tar <wladomania@wp.pl>
2024-10-24 15:15:06 +0200

    added logo icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
-import ThemeSwitch from './ThemeSwitch';
+import React from "react";
+import ThemeSwitch from "./ThemeSwitch";
+import Link from "next/link";
+import { LogoIcon } from "./Icons";
 
 const Navbar = () => {
   return (
-    <div className='mx-auto max-w-5xl px-6'>
-        <div className='flex justify-between items-center h-16 w-full'>
-            <Link href='/'><div>Some logo</div></Link>
-            <ThemeSwitch />
-        </div>
+    <div className="mx-auto max-w-5xl px-6">
+      <div className="flex justify-between items-center h-16 w-full">
+        <Link href="/">
+          <div >
+            <LogoIcon />
+          </div>
+        </Link>
+        <ThemeSwitch />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
